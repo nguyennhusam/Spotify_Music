11853f2b5add85cf9b98ed58167c6c3d15cdd96f
nguyennhusam <alpkenrun142@gmail.com>
2023-03-18 13:10:55 +0700

    task menu

--- app/src/main/java/hcmute/edu/vn/musicmediaplayer/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/musicmediaplayer/MainActivity.java
@@ -2,7 +2,12 @@ package hcmute.edu.vn.musicmediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
