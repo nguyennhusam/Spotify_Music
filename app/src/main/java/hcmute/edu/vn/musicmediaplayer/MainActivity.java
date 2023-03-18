5e01d6621429b1e1c6049cdee451f04b39721327
nguyennhusam <alpkenrun142@gmail.com>
2023-03-18 22:51:05 +0700

    completed task menu

--- app/src/main/java/hcmute/edu/vn/musicmediaplayer/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/musicmediaplayer/MainActivity.java
@@ -1,20 +1,80 @@
 package hcmute.edu.vn.musicmediaplayer;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStateManagerControl;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager2 mViewPager2;
+    private BottomNavigationView mBottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewPager2 = findViewById(R.id.view_pager);
+        mBottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        mViewPager2.setAdapter(adapter);
+
+        mViewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                switch (position) {
+                    case 0:
+                        mBottomNavigationView.getMenu().findItem(R.id.menu_tab_home).setChecked(true);
+                        break;
+                    case 1:
+                        mBottomNavigationView.getMenu().findItem(R.id.menu_tab_search).setChecked(true);
+                        break;
+                    case 2:
+                        mBottomNavigationView.getMenu().findItem(R.id.menu_tab_lib).setChecked(true);
+                        break;
+                }
+            }
+        });
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_tab_home:
+                        mViewPager2.setCurrentItem(0);
+                        break;
+                    case R.id.menu_tab_search:
+                        mViewPager2.setCurrentItem(1);
+                        break;
+                    case R.id.menu_tab_lib:
+                        mViewPager2.setCurrentItem(2);
+                        break;
+                }
+                return true;
+            }
+        });
     }
+
+
 
 }
---
+++ app/src/main/java/hcmute/edu/vn/musicmediaplayer/ViewPagerAdapter.java
@@ -0,0 +1,34 @@
+package hcmute.edu.vn.musicmediaplayer;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new SearchFragment();
+            case 2:
+                return new LibraryFragment();
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
