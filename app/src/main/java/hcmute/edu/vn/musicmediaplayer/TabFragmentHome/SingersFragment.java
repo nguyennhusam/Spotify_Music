d3893b1ea1117864c0e46ac6deb8e27b7a856de4
nguyennhusam <alpkenrun142@gmail.com>
2023-04-18 06:07:50 +0700

    hoàn thành tab home <fill list nhạc và phát nhạc>

--- app/src/main/java/hcmute/edu/vn/musicmediaplayer/TabFragmentHome/SingersFragment.java
+++ app/src/main/java/hcmute/edu/vn/musicmediaplayer/TabFragmentHome/SingersFragment.java
@@ -4,19 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import hcmute.edu.vn.musicmediaplayer.MainActivity;
+import hcmute.edu.vn.musicmediaplayer.Model.Song;
 import hcmute.edu.vn.musicmediaplayer.R;
 
 public class SingersFragment extends Fragment {
+    LinearLayout layoutMono;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    CircleImageView imageView1;
+    CircleImageView imageView2;
+    CircleImageView imageView3;
+    CircleImageView imageView4;
+    CircleImageView imageView5;
+    CircleImageView imageView6;
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -45,7 +62,42 @@ public class SingersFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_singers, container, false);
+        View view = inflater.inflate(R.layout.fragment_singers, container, false);
+
+        CircleImageView imageView1 = view.findViewById(R.id.image_view_sontung);
+        Glide.with(this)
+                .load(R.drawable.sontungmpt)
+                .into(imageView1);
+
+        CircleImageView imageView2 = view.findViewById(R.id.image_view_mono);
+        Glide.with(this)
+                .load(R.drawable.mono)
+                .into(imageView2);
+
+        CircleImageView imageView3 = view.findViewById(R.id.image_view_hoangdung);
+        Glide.with(this)
+                .load(R.drawable.hoangdung)
+                .into(imageView3);
+
+        CircleImageView imageView4 = view.findViewById(R.id.image_view_justinbieber);
+        Glide.with(this)
+                .load(R.drawable.justinbieber)
+                .into(imageView4);
+
+        CircleImageView imageView5 = view.findViewById(R.id.image_view_taylorswift);
+        Glide.with(this)
+                .load(R.drawable.taylorswift)
+                .into(imageView5);
+
+        CircleImageView imageView6 = view.findViewById(R.id.image_view_charlieputh);
+        Glide.with(this)
+                .load(R.drawable.charlieputh)
+                .into(imageView6);
+
+
+
+
+
+        return view;
     }
 }
